71051b36e5637c4f8b0e7b72540647347f206b7a
Igor Kovalev <grkovalev@gmail.com>
2016-08-07 01:01:31 +0400

    Add some comments for video.py. Rename class movie to Movie

--- favourites_video.py
+++ favourites_video.py
@@ -1,3 +1,4 @@
+
 import video
 import fresh_tomatoes
 
@@ -5,17 +6,17 @@ import fresh_tomatoes
 
 
 # Instance of movie
-four_rooms = video.movie("Four Rooms",
+four_rooms = video.Movie("Four Rooms",
                           120,
                           "https://upload.wikimedia.org/wikipedia/en/c/c8/Four_rooms_ver2.jpg",
                           "https://www.youtube.com/watch?v=Rieq_TR7cV0")
 
-skool_of_rock = video.movie("School of Rock",
+skool_of_rock = video.Movie("School of Rock",
                              109 ,
                              "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", 
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
-the_fith_element = video.movie('The Fith Element',
+the_fith_element = video.Movie('The Fith Element',
                                 126,
                                 "https://upload.wikimedia.org/wikipedia/en/6/65/Fifth_element_poster_%281997%29.jpg",
                                 "https://www.youtube.com/watch?v=fQ9RqgcR24g")
--- video.py
+++ video.py
@@ -1,5 +1,15 @@
-class movie():
-    def __init__(self,title,duration,poster_url,trailer_url):
+# contains data structure for createion web 
+# page elements by fresh_tomatoes.py
+
+class Movie():
+    def __init__(self, title, duration, poster_url, trailer_url):
+        """Init metohd 
+            Keyword arguments:
+            title       -- title of the movie
+            duration    -- duration of the Movie
+            poster_url  -- url with poster picture
+            trailer_url -- url with trailer video 
+        """      
         self.title = title
         self.duration = duration
         self.poster_image_url = poster_url
